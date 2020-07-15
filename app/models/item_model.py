23a622016187b3243a5a28f2cb3127bd30bda9eb
Peter Arnold <peter@entroprise.com>
2020-07-15 12:12:54 -0400

    asd

--- app/models/item_model.py
+++ app/models/item_model.py
@@ -1,10 +1,41 @@
 from pydantic import BaseModel
-
-class Item(BaseModel):
-    id: str
-    question: str
-    answer: str
-    content: str
+from typing import Optional
 
 class Question(BaseModel):
     question: str
+
+class ItemData(BaseModel):
+    id: str
+    content: str
+    question: str
+    answer: str
+
+class DeliveryInfo(BaseModel):
+    max_retries: int
+    current_retry: int
+
+class Trigger(BaseModel):
+    name: str
+
+class Table(BaseModel):
+    name: str
+
+# class SessionVariables(BaseModel):
+#     x-hasura-role: str
+
+class Data(BaseModel):
+    old: Optional[ItemData] = None
+    new: Optional[ItemData] = None
+
+class Event(BaseModel):
+    # session_variables: Optional[SessionVariables] = None
+    op: str
+    data: Optional[Data] = None
+
+class Item(BaseModel):
+    event: Optional[Event] = None
+    created_at: str
+    id: str
+    delivery_info: Optional[DeliveryInfo] = None
+    trigger: Optional[Trigger] = None
+    table: Optional[Table] = None
