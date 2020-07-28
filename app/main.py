a200249ffdeac9f78ccaf2f66ef7d31d49dc4c39
peterentroprise <67916514+peterentroprise@users.noreply.github.com>
2020-07-28 14:12:54 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -21,7 +21,7 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origin_regex='https?://.*',
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
